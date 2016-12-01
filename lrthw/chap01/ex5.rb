6f24a5273e27902ed89181403b3901379c8edf0c
John Ashton <John_ashton@hotmail.com>
2016-12-01 16:57:28 +0000

    Exercise 5

---
+++ lrthw/chap01/ex5.rb
@@ -0,0 +1,23 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk abut #{name}"
+puts "He's #{height} inches tall"
+puts "He's #{weight} pounds heavy"
+puts "Actually that's not too heavy"
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age} , #{height} , and #{weight} I get #{age + height + weight}."
+
+
+cm_height = height * 2.54 #height converted to cm
+kg_weight = weight * 0.45 #weight converted to kg
+
+puts "I am #{cm_height} tall in centimeters."
+puts "i am #{kg_weight} heavy in kilograms."
