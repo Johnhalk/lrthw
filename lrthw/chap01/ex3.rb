62554e9bd7148699d9c2ebe486c7ca75b96b70f9
John Ashton <John_ashton@hotmail.com>
2016-12-01 16:20:42 +0000

    Exercise 3

---
+++ lrthw/chap01/ex3.rb
@@ -0,0 +1,40 @@
+puts 2 + 3 #Addition - 5
+puts 2 - 1 #Subtraction - 1
+puts 6 / 2 #Division - 3
+puts 2 * 5 #multiplication - 10
+puts 10 % 2 #modulo - 0 (no remainder)
+puts 5 < 10 #less than to return true
+puts 5 > 7 #greater than, to return false
+puts 4 <= 6 #less than or equal to return True
+puts 5 >= 5 #greater than or equal to
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 /6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5-7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal #{5 <= -2}"
+
+puts 3.4 + 6.8
+puts 2.6 / 1.3
+puts 5.2 * 4
+puts 5.6 % 2.4
+puts 7 < 3.4
+puts 7.8 >= 8.22
