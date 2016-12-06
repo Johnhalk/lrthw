f4e14af42c281bbb1910b7557311e270672bf0c5
John Ashton <John_ashton@hotmail.com>
2016-12-06 12:08:43 +0000

    Exercise 27 - Truth terms

---
+++ lrthw/chap01/ex27.rb
@@ -0,0 +1,14 @@
+#Exercise 27 - The truth terms
+
+#In Ruby we have the following terms (characters and phrases) for determining if something
+# is "true" or "false." Logic on a computer is all about seeing if some combination of these characters and some variables is true at that point in the program.
+
+&& - (and)
+|| - (or)
+! - (not)
+!= - (not equal)
+== (equal)
+>= (greater-than-equal)
+<= (less-than-equal)
+true
+false
