0ff9fccccaf9640831a64f11ab435ead72e557ec
John Ashton <John_ashton@hotmail.com>
2016-12-06 16:50:35 +0000

    Exercise 34 - Accessing Elements of Arrays

---
+++ lrthw/chap01/ex34.rb
@@ -0,0 +1,19 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#The animal at 1 is ruby, though its not an animal...
+#the third (3rd) animal is a peacock in ordinal, and position 2 in cardinal
+#the first (1st) animal is a bear in ordinal, and in position 0 in cardinal
+#the animal at 3 is kangaroo in cardinal, and is fourth in ordinal.
+#the fifth (5th) animal is whale in ordinal and in position 4 in cardinal.
+#the animal at 2 is peacock in cardinal, and 3rd in ordinal
+#the sixth (6th) animal is platypus in ordinal and in position 5 in cardinal.
+#the animal at 4 is whale in ordinal and in position 5 in cartdinal.
+
+
+colour = ['blue' ,'green' , 'yellow' , 'orange' , 'pink']
+
+#the first colour is at 0 and it is blue
+#the second colour is at 1 and it is green
+#the third colour is at 2 and it is yellow
+#the fourth colour is at 3 and it is orange
+#the fifth colour is at 4 and it is pink
